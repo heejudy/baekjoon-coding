12c83209f329dba47dc13858f452072bc4d287f6
heejudy <moheeju1223@gmail.com>
2025-09-13 15:37:32 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 180 ms, Memory: 17676 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,14 @@
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+
+        int N = scanner.nextInt();
+
+        for (int i = 1; i <= N; i++){
+            System.out.println("*".repeat(i));
+        }
+    }
+}
