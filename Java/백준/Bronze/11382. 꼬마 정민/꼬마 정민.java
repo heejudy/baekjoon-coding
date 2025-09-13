c9a13bc585439a180e51a70bf2bafe52828bb7cd
heejudy <moheeju1223@gmail.com>
2025-09-13 15:40:22 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14324 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader((System.in)));
+        String[] array = bf.readLine().split(" ");
+
+        long a = Long.parseLong(array[0]);
+        long b = Long.parseLong(array[1]);
+        long c = Long.parseLong(array[2]);
+
+        System.out.println(a + b + c);
+    }
+}
