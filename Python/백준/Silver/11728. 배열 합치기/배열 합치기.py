52fe0ef31473f3aadc57222e51e06fdbb72948b7
heejudy <moheeju1223@gmail.com>
2025-09-21 16:53:04 +0900

    [Silver V] Title: 배열 합치기, Time: 2068 ms, Memory: 188024 KB -BaekjoonHub

--- Python/백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ Python/백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -7,17 +7,17 @@ first = list(map(int, input().split()))
 second = list(map(int, input().split()))
 
 i, j = 0, 0
-while (i < len(first) and j < len(second)):
+while (i < a and j < b):
     if first[i] < second[j]:
         total.append(first[i])
         i += 1
     else: 
         total.append(second[j])
         j += 1
-if (i <= len(first)):
-    total.extend(first[i:len(first)])
-if (j <= len(second)):
-    total.extend(second[j:len(second)])
+if (i <= a):
+    total.extend(first[i:a])
+if (j <= b):
+    total.extend(second[j:b])
 
 for i in total:
     print(i)
