ebae0c69a407b3a0c1eb0bbbcd156be3dd0c85cb
heejudy <moheeju1223@gmail.com>
2026-02-04 12:26:41 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ Python/백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,11 +1,11 @@
-a = int(input())
-lst = [0 for i in range(a + 1)]
+num = int(input())
 
-def fib(n):
-    if n > 0:
-        lst[1] = 1
-        for i in range(2, n + 1):
-            lst[i] = lst[i - 1] + lst[i - 2]
-    return lst[n]
-    
-print(fib(a))
+lst = [0 for i in range(num + 1)]
+
+for i in range(num + 1):
+    if i == 0 or i == 1:
+        lst[i] = i
+    else:
+        lst[i] = lst[i - 1] + lst[i - 2]
+
+print(lst[-1])
