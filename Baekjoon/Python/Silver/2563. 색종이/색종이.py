22ce80ff12bdc04cb6b8c83646e3bd717b284ea2
heejudy <moheeju1223@gmail.com>
2026-03-05 16:10:08 +0900

    git push

---
+++ Baekjoon/Python/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,13 @@
+paper = [[0]*100 for _ in range(100)]
+
+for _ in range(int(input())):
+    x, y = list(map(int, input().split()))
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
+            paper[i][j] = 1
+    
+result = 0
+for i in range(100):
+    result += sum(paper[i])
+
+print(result)
