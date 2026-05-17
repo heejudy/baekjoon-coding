e4c01f63ed3059448584f73cd69b0d5f4f0d0709
heejudy <moheeju1223@gmail.com>
2026-05-17 19:25:38 +0900

    [level 2] Title: 오픈채팅방, Time: 68.27 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,26 @@
+def solution(record):
+    answer = []
+    chat = {}
+    
+    for i in record:
+        record_status = i.split()
+        # Enter 
+        if record_status[0] == 'Enter':
+            user_id, name = record_status[1], record_status[2]
+            chat[user_id] = name
+        elif record_status[0] == 'Leave':
+            user_id = record_status[1]
+        else:
+            user_id, name = record_status[1], record_status[2]
+            chat[user_id] = name
+            
+    for i in record:
+        record_status = i.split()
+        user_id = record_status[1]
+        name = chat[user_id]
+        if record_status[0] == "Enter":
+            answer.append(f"{name}님이 들어왔습니다.")
+        elif record_status[0] == "Leave":
+            answer.append(f"{name}님이 나갔습니다.")
+
+    return answer
